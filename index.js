e4689797a59eab17fc4f521f0e8cf0c973ec410f
Michel Deutou <oreyimavinci@gmail.com>
2026-04-05 23:34:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.all("/voice", (req, res) => {
-  res.type("text/xml");
-  res.send(`
-    <Response>
-      <Say>Hello, thank you for calling. How can I help you?</Say>
-    </Response>
-  `);
+  res.set("Content-Type", "text/xml");
+
+  res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Say>Hello, thank you for calling. How can I help you?</Say>
+</Response>`);
 });
